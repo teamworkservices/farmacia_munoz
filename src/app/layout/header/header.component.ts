ee3b86d89bbd5785a3892fc5c021d31c7e606c0b
juanjoseMM95 <jose.munoz@opendeusto.es>
2021-12-15 10:30:17 -0500

    maquetado-inicial
    Se suben cambios de maquetado inicial.

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,19 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+      
+  }
+}
